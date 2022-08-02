8f93d02b5bf06aa80dea1025a999ed8614d116e9
BooteVoid <101889442+BooteVoid@users.noreply.github.com>
2022-08-01 22:50:45 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function preload() {
   video = createVideo('video.mp4');
@@ -29,4 +30,31 @@ function modelLoaded() {
   status = true;
   objectDetector.detect(img,gotResult);
   
+}
+
+function gotResult (error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+function draw () {
+  image(video, 0, 0, 480, 380);
+  if(objects[i].label == object_name)
+  {
+    objectDetector.detect(gotResult);
+    for (i = 0; i < objects.length; i++) {
+      document.getElementById("object_status").innerHTML = object_name + "Found"
+      
+      fill('#FF0000');
+      percent = floor(objects[i].confidence*100);
+      text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+      noFill();
+      stroke("#FF0000");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
+
 }
